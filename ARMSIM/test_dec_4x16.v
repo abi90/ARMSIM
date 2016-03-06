5572b6319064e062a800eacc753027e2ebc6ef29
abi90 <abisai.ramos@upr.edu>
2016-03-06 09:30:41 -0400

    Fixed test_dec_4x16.v problems.

--- ARMSIM/test_dec_4x16.v
+++ ARMSIM/test_dec_4x16.v
@@ -3,18 +3,18 @@ reg  [3:0] I;
 wire [15: 0] Y; //Las salidas deben ser tipo wire
 reg enable;
 
-parameter sim_time = 1000;
+parameter sim_time = 10000;
 decoder_4x16 dec (Y, I, enable);
 initial #sim_time $finish; // Especifica cuando termina simulación
 
 initial begin
 I = 2'b00; //Genera las combinaciones de las entradas
-repeat (16) #16 I=I+1'b1; //cada 10 unidades de tiempo
+repeat (31) #16 I=I+1'b1; //cada 10 unidades de tiempo
 end
 
 always @(I)
 begin
-if(I === 4'b1111) begin
+if(I === 4'b0000) begin
 enable = ~enable;
 end
 end
