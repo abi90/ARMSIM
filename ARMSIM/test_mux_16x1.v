4d1b012f1a0f960432e94b49aa2e308be9abc829
carlboy03 <carlboy03@Carlos-Fuentes-MacBook-Pro.local>
2016-03-06 18:51:25 -0400

    Committing register file
    Committing register file and scripts and test for mx_16x1

--- ARMSIM/test_mux_16x1.v
+++ ARMSIM/test_mux_16x1.v
@@ -17,12 +17,12 @@ reg  [31: 0] I13; //Las entradas del módulo deben ser tipo reg
 reg  [31: 0] I14; //Las entradas del módulo deben ser tipo reg
 reg  [31: 0] I15; //Las entradas del módulo deben ser tipo reg
 wire [31: 0] Y; //Las salidas deben ser tipo wire
-reg [1:0] S;
+reg [3:0] S;
 
 
 parameter sim_time = 1000;
 
-mux_16_1 mux2 (Y, S, I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15);
+mux_16x1 mux2 (Y, S, I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15);
 
 initial #sim_time $finish; // Especifica cuando termina simulación
 initial begin
@@ -46,7 +46,7 @@ S = 4'b0000;
 repeat (16) #16 S=S+1'b1;
 end
 initial begin
-$display (" S \t I3 \t I2 \t I1 \t I0 \t Y  "); //imprime header
-$monitor (" %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h ", S, I15, I14, I13, I12, I11, I10, I9, I8, I7, I6, I15, I4, I3, I2, I1, I0 Y); //imprime las señales
+$display (" S \t I15 \t I14 \t I13 \t I12 \t I11 \t I10 \t I9 \t I8 \t I7 \t I6 \t I5 \t I4 \t I3 \t I2 \t I1 \t I0 \t Y  "); //imprime header
+$monitor (" %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h %h ", S, I15, I14, I13, I12, I11, I10, I9, I8, I7, I6, I15, I4, I3, I2, I1, I0 , Y); //imprime las señales
 end
 endmodule
