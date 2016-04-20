c143549b906508c4e4c0d897055bd9b35887be32
abi90 <abisai.ramos@upr.edu>
2016-04-20 01:27:23 -0400

    Pipeline_Register.v

--- ARMSIM/Pipeline_Register.v
+++ ARMSIM/Pipeline_Register.v
@@ -4,7 +4,7 @@
 
 //Verificar el clock
 
-module PipelineRegister (output reg [54:0] out, input [54:0] pipein ,input clk)
+module Pipeline_Register (output reg [54:0] out, input [54:0] pipein ,input clk);
 
 always @(posedge clk)
 	out <= pipein;
